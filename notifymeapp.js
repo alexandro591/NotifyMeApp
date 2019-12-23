02147f6c3b480036c8ac30a53960427b28c3b2de
“alexandro591” <alexandrotapiaflores@gmail.com>
2019-12-22 20:22:26 -0500

    second version

--- notifymeapp.js
+++ notifymeapp.js
@@ -1,11 +1,11 @@
 function notifymeapp(email,app,notification) {
 	const data = { 'email': email, 'app': app, 'notification': notification };
-	const urlp1="https://notifymeapp.netlify.com/url.php?"
+	const urlp1="https://notifymeapp.netlify.com/url.html?"
 	const ret = [];
 	for (let d in data)
 		ret.push(encodeURIComponent(d) + '=' + encodeURIComponent(data[d]));
 	url = urlp1+ret.join('&');
-	$.get(url);
+	window.open(url, '__blank')
 }
 
 
